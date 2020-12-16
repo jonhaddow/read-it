07e965c977772692727d828c4ce7cb1b7b752c1c
Jon Haddow <haddowjon@protonmail.com>
2020-12-16 20:12:30 +0000

    Reset tables for tests

--- src/tests/bookmarks.test.ts
+++ src/tests/bookmarks.test.ts
@@ -1,5 +1,25 @@
+import config from "config";
+import { Pool, PoolClient } from "pg";
 import request from "supertest";
 import { app, server } from "../app";
+
+const pool = new Pool(config.get("db_config"));
+
+beforeAll(async () => {
+	let client: PoolClient | null = null;
+	try {
+		client = await pool.connect();
+		await client.query("DROP TABLE bookmarks");
+		await client.query(`
+			CREATE TABLE bookmarks (
+				id char(36),
+				url varchar(2000)
+			)
+		`);
+	} finally {
+		client?.release();
+	}
+});
 
 describe("bookmarks", () => {
 	it("should return empty list of bookmarks", async () => {
