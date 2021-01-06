82626e5f2a04d3489c6932d2ebfd20df9a67e5bc
Jon Haddow <haddowjon@protonmail.com>
2021-01-06 20:49:44 +0000

    Ensure puppeteer is running without sandbox
    https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-on-heroku

--- src/subscribers/populateBookmark.ts
+++ src/subscribers/populateBookmark.ts
@@ -2,7 +2,7 @@ import { Bookmark } from "../entities";
 import puppeteer from "puppeteer";
 
 export const populateBookmark = async (data: Bookmark): Promise<void> => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
 	const page = await browser.newPage();
 	await page.goto(data.url);
 
