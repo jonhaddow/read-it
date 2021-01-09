afd38cce2497d5fa115eb135b96648577307cb47
Jon Haddow <haddowjon@protonmail.com>
2021-01-09 19:06:00 +0000

    Wrap url scaping process with try/catch

--- src/subscribers/populateBookmark.ts
+++ src/subscribers/populateBookmark.ts
@@ -3,23 +3,26 @@ import puppeteer from "puppeteer";
 import { getRepository } from "typeorm";
 
 export const populateBookmark = async (bookmark: Bookmark): Promise<void> => {
-	const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-	const page = await browser.newPage();
-	await page.goto(bookmark.url);
+	try {
+		const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+		const page = await browser.newPage();
+		await page.goto(bookmark.url);
 
-	const titleContent = await page.$eval('[property="og:title"]', (node) =>
-		node.getAttribute("content")
-	);
+		const titleContent = await page.$eval('[property="og:title"]', (node) =>
+			node.getAttribute("content")
+		);
+		if (titleContent) bookmark.title = titleContent;
 
-	const descriptionContent = await page.$eval(
-		'[property="og:description"]',
-		(node) => node.getAttribute("content")
-	);
+		const descriptionContent = await page.$eval(
+			'[property="og:description"]',
+			(node) => node.getAttribute("content")
+		);
+		if (descriptionContent) bookmark.description = descriptionContent;
 
-	if (titleContent) bookmark.title = titleContent;
-	if (descriptionContent) bookmark.description = descriptionContent;
+		await getRepository(Bookmark).save(bookmark);
 
-	await getRepository(Bookmark).save(bookmark);
-
-	await browser.close();
+		await browser.close();
+	} catch (ex) {
+		console.error("Failed to populate bookmarks", ex);
+	}
 };
