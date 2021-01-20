ba7ee43adcea16dcc7e2b0e7b2f990560372552d
Jon Haddow <haddowjon@protonmail.com>
2021-01-20 20:36:21 +0000

    test: mock reddit client

--- src/tests/bookmarks_populate_reddit.test.ts
+++ src/tests/bookmarks_populate_reddit.test.ts
@@ -3,6 +3,22 @@ import { SuperAgentTest } from "supertest";
 import { Bookmark } from "../entities";
 import { createSuperAgent, startTestServer, stopTestServer } from "./utils";
 import { populateBookmark } from "../subscribers/populateBookmark";
+import { Submission } from "snoowrap";
+
+// Mocking the reddit API response.
+jest.mock("snoowrap", () => {
+	return jest.fn().mockImplementation(() => {
+		return {
+			getSubmission: jest.fn().mockReturnValue({
+				fetch: jest.fn().mockResolvedValue({
+					title: "Reddit Title",
+					url: "https://www.eff.org",
+					thumbnail: "https://thumbnail.test.com",
+				} as Submission),
+			}),
+		};
+	});
+});
 
 describe("bookmarks_populate_reddit", () => {
 	let agent: SuperAgentTest;
@@ -17,9 +33,9 @@ describe("bookmarks_populate_reddit", () => {
 	});
 
 	[
-		"https://www.reddit.com/r/news/comments/kx9xx9/a_man_who_carried_a_confederate_flag_into_the/",
-		"https://www.reddit.com/kx9xx9",
-		"https://redd.it/kx9xx9",
+		"https://www.reddit.com/r/news/comments/123456/more_content/",
+		"https://www.reddit.com/123456",
+		"https://redd.it/123456",
 	].forEach((x) => {
 		describe(`when the URL is ${x}`, () => {
 			let bookmark: Bookmark;
@@ -41,18 +57,12 @@ describe("bookmarks_populate_reddit", () => {
 				const response = await agent.get(`/api/bookmarks/${bookmark.id}`);
 				bookmark = response.body as Bookmark;
 
-				expect(bookmark.title).toEqual(
-					"A man who carried a Confederate flag into the Capitol has been arrested."
-				);
+				expect(bookmark.title).toEqual("Reddit Title");
 				expect(bookmark.description).toEqual(
-					"A federal prosecutor said that a retired Air Force officer who stormed the Senate chamber holding zip ties had intended to “take hostages.”"
+					"Defending your rights in the digital world"
 				);
-				expect(bookmark.thumbnailUrl).toEqual(
-					"https://static01.nyt.com/images/2021/01/14/us/14transition-briefing-confederate-flag-arrest/14transition-briefing-confederate-flag-arrest-facebookJumbo-v2.jpg"
-				);
-				expect(bookmark.targetURL).toEqual(
-					"https://www.nytimes.com/2021/01/14/us/Kevin-Seefried-arrested.html"
-				);
+				expect(bookmark.thumbnailUrl).toEqual("https://thumbnail.test.com");
+				expect(bookmark.targetURL).toEqual("https://www.eff.org");
 				expect(bookmark.specialType).toEqual("reddit");
 			});
 		});
