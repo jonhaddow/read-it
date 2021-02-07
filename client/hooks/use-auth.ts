f8b20e02c17ad1de8c7c4165fc35f9a87e56c1e0
Jon Haddow <haddowjon@protonmail.com>
2021-02-07 21:56:51 +0000

    chore: remove unused eslint ignore

--- client/hooks/use-auth.ts
+++ client/hooks/use-auth.ts
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars -- seems like a bug to me :(
 import { authContext, AuthContextValue } from "../services";
 
 export const useAuth = (): AuthContextValue | undefined => {
