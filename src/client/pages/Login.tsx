1b6dcfd5a8cc4f3b0d424d28bf7501aae3de24c0
Jon Haddow <haddowjon@protonmail.com>
2021-02-17 19:55:24 +0000

    fix: login links to use server url

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -6,10 +6,10 @@ export const Login: React.FC = () => {
 			<h1>Login methods</h1>
 			<ul>
 				<li>
-					<a href="/api/auth/google">Login with Google</a>
+					<a href={`${API_URL}/api/auth/google`}>Login with Google</a>
 				</li>
 				<li>
-					<a href="/api/auth/github">Login with Github</a>
+					<a href={`${API_URL}/api/auth/github`}>Login with Github</a>
 				</li>
 			</ul>
 		</>
