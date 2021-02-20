c05025dd3b8a71b1851b0d0412d6bfe60b6c6b01
Jon Haddow <haddowjon@protonmail.com>
2021-02-20 21:08:00 +0000

    chore: disable eslint typescript rules for JS files

--- src/client/.eslintrc.js
+++ src/client/.eslintrc.js
@@ -7,4 +7,13 @@ module.exports = {
 	rules: {
 		"no-console": "warn",
 	},
+	overrides: [
+		{
+			// disable the typescript rules specifically for JS files
+			files: ["*.js", "*.jsx"],
+			rules: {
+				"@typescript-eslint/explicit-function-return-type": "off",
+			},
+		},
+	],
 };
