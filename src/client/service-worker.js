4d7aad77190543cca9b2d2630d47e4bf2f03c709
Jon Haddow <haddowjon@protonmail.com>
2021-02-25 22:05:29 +0000

    fix(client): use register route to simplify share target registration

--- src/client/service-worker.js
+++ src/client/service-worker.js
@@ -6,6 +6,7 @@ import {
 } from "workbox-recipes";
 import { precacheAndRoute } from "workbox-precaching";
 import fetch from "node-fetch";
+import { registerRoute } from "workbox-routing";
 
 // Include offline.html in the manifest
 precacheAndRoute(self.__WB_MANIFEST);
@@ -20,20 +21,17 @@ offlineFallback();
 
 // Handle POST to /bookmark which is registered in the manifest as a share target.
 // When received, grab the info and pass it to the server.
-self.addEventListener("fetch", (event) => {
-	const url = new URL(event.request.url);
-	if (event.request.method === "POST" && url.pathname === "/bookmark") {
-		event.respondWith(
-			(async () => {
-				const formData = await event.request.formData();
-				const url = formData.get("url") || formData.get("title");
-				// eslint-disable-next-line no-undef
-				await fetch(`${API_URL}/api/bookmark`, {
-					method: "POST",
-					body: JSON.stringify({ url }),
-				});
-				return Response.redirect("/", 303);
-			})()
-		);
-	}
-});
+const shareTargetHandler = async ({ event }) => {
+	const formData = await event.request.formData();
+	const url = formData.get("url") || formData.get("title");
+	// eslint-disable-next-line no-undef
+	await fetch(`${API_URL}/api/bookmark`, {
+		method: "POST",
+		body: JSON.stringify({ url }),
+	});
+
+	// After the POST succeeds, redirect to the main page.
+	return Response.redirect("/", 303);
+};
+
+registerRoute("/_share-target", shareTargetHandler, "POST");
