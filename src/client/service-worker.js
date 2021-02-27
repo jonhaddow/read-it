0c60e0740fd49eb42a6940ad7a11d5ec44b07241
Jon Haddow <haddowjon@protonmail.com>
2021-02-27 20:56:47 +0000

    refactor(client): deregister unused routes in service worker

--- src/client/service-worker.js
+++ src/client/service-worker.js
@@ -5,37 +5,18 @@ import {
 	offlineFallback,
 } from "workbox-recipes";
 import { precacheAndRoute } from "workbox-precaching";
-import fetch from "node-fetch";
-import { registerRoute } from "workbox-routing";
 
 // Include offline.html in the manifest
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Respond to page requests with a network first caching strategy
 pageCache();
 
+// Respond to CSS, JavaScript, and Web Worker requests with a stale-while-revalidate strategy
 staticResourceCache();
 
+// Respond to image requests with a cache first strategy
 imageCache();
 
+// Provide an offline fallback for page requests (offline.html)
 offlineFallback();
-
-// Handle POST to /bookmark which is registered in the manifest as a share target.
-// When received, grab the info and pass it to the server.
-const shareTargetHandler = async ({ event }) => {
-	const formData = await event.request.formData();
-
-	// No clue what platform is going to send what parameter...
-	const url =
-		formData.get("url") || formData.get("title") || formData.get("text");
-
-	// eslint-disable-next-line no-undef
-	await fetch(`${API_URL}/api/bookmark`, {
-		method: "POST",
-		body: JSON.stringify({ url }),
-	});
-
-	// After the POST succeeds, redirect to the main page.
-	return Response.redirect("/", 303);
-};
-
-registerRoute("/_share-target", shareTargetHandler, "POST");
