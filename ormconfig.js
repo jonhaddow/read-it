bcfb58bef131dee2b3e151c264ffd93bf63b443e
Jon Haddow <haddowjon@protonmail.com>
2021-03-12 21:49:37 +0000

    Configure ormconfig to use src files for entities when running in development

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,33 @@
-module.exports = {
-	type: "postgres",
-
-	host: process.env.DB_HOST || "localhost",
-	database: process.env.DB_NAME || "postgres",
-	username: process.env.DB_USER || "postgres",
-	password: process.env.DB_PASS || "postgres",
-	port: process.env.DB_PORT || 5432,
-
-	entities: ["./src/server/entities/*.ts"],
-	migrations: ["./src/server/migrations/*.js"],
-	migrationsRun: true,
-
-	logging: ["error", "warn"],
-
-	cli: {
-		entitiesDir: "./src/server/entities",
-		migrationsDir: "./src/server/migrations",
-	},
+const dev = {
+	entities: ["src/server/entities/*.ts"],
+	migrations: ["src/server/migrations/*.js"],
 };
+
+const prod = {
+	entities: ["dist/server/entities/*.js"],
+	migrations: ["dist/server/migrations/*.js"],
+};
+
+const isTS =
+	process[Symbol.for("ts-node.register.instance")] ||
+	process.env.JEST_WORKER_ID;
+
+module.exports = Object.assign(
+	{
+		type: "postgres",
+
+		host: process.env.DB_HOST || "localhost",
+		database: process.env.DB_NAME || "postgres",
+		username: process.env.DB_USER || "postgres",
+		password: process.env.DB_PASS || "postgres",
+		port: process.env.DB_PORT || 5432,
+
+		migrationsRun: true,
+		logging: ["error", "warn"],
+		cli: {
+			entitiesDir: "./src/server/entities",
+			migrationsDir: "./src/server/migrations",
+		},
+	},
+	isTS ? dev : prod
+);
