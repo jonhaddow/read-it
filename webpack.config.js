68caace61563a0d563a2c6daba6f17b390020db4
Jon Haddow <haddowjon@protonmail.com>
2021-07-19 22:07:08 +0100

    Revert "Use esbuild-loader in client side webpack"
    This reverts commit 45ab42910825e21d274c000b98e8c0f35f7a374c.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const { DefinePlugin } = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { ESBuildMinifyPlugin } = require("esbuild-loader");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = ({ production }) => {
 	const plugins = [
@@ -56,10 +56,14 @@ module.exports = ({ production }) => {
 					test: /\.tsx?$/,
 					exclude: /(node_modules|bower_components)/,
 					use: {
-						loader: "esbuild-loader",
+						loader: "babel-loader",
 						options: {
-							loader: "tsx",
-							target: "esnext",
+							cacheDirectory: true,
+							presets: [
+								"@babel/preset-env",
+								"@babel/preset-react",
+								"@babel/preset-typescript",
+							],
 						},
 					},
 				},
@@ -96,12 +100,7 @@ module.exports = ({ production }) => {
 		},
 		plugins,
 		optimization: {
-			minimizer: [
-				new ESBuildMinifyPlugin({
-					target: "esnext",
-					css: true, // Apply minification to CSS assets
-				}),
-			],
+			minimizer: [`...`, new CssMinimizerPlugin()],
 		},
 	};
 };
