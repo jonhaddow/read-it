02d34cd1204b2ba0154ed37f9ff1a3714e365def
Jon Haddow <jon@haddow.me>
2021-09-11 20:14:59 +0100

    Improve styling of login page

--- src/client/pages/Login.tsx
+++ src/client/pages/Login.tsx
@@ -1,27 +1,44 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { Location } from "history";
+import {
+	GoogleLoginButton,
+	GithubLoginButton,
+} from "react-social-login-buttons";
 
 export const Login: React.FC = () => {
 	const from = useLocation<{ from?: Location } | undefined>()?.state?.from;
+	const returnTo = from ? encodeURIComponent(from.pathname + from.search) : "/";
 
 	return (
-		<>
-			<h1>Login methods</h1>
-			<ul>
-				<li>
-					<a
-						href={`${API_URL}/api/auth/google?returnTo=${
-							from ? encodeURIComponent(from.pathname + from.search) : "/"
-						}`}
-					>
-						Login with Google
-					</a>
-				</li>
-				<li>
-					<a href={`${API_URL}/api/auth/github`}>Login with Github</a>
-				</li>
-			</ul>
-		</>
+		<div className="flex justify-center items-center h-screen">
+			<div className="shadow-xl p-4">
+				<h1 className="font-bold text-lg mb-4 text-center">
+					<span role="img" aria-label="alligator">
+						🐊
+					</span>{" "}
+					Alligator
+				</h1>
+				<p className="text-sm font-light text-center mb-4">
+					Save links from across the web.
+				</p>
+				<ul>
+					<li>
+						<GoogleLoginButton
+							onClick={() =>
+								(window.location.href = `${API_URL}/api/auth/google?returnTo=${returnTo}`)
+							}
+						/>
+					</li>
+					<li>
+						<GithubLoginButton
+							onClick={() =>
+								(window.location.href = `${API_URL}/api/auth/github?returnTo=${returnTo}`)
+							}
+						/>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
 };
